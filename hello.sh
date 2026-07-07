22bbb2392b2f4d03584aaea045c42656183fb3bc
PedroViniciusMD <pedrodelgado583@gmail.com>
2026-07-07 18:01:58 -0300

    first commit

---
+++ hello.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+echo "Hello, World!"
+
+#System variable examples
+echo $BASH
+echo $BASH_VERSION
+echo $HOME
+echo $PWD
+
+#User-defined variable examples
+name=Pedro
+age=00
+echo "My name is $name"
+echo "My age is $age"

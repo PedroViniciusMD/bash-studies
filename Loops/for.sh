83a5e23d463eca7016aa6d56768cbafc8c7f82ec
PedroViniciusMD <pedrodelgado583@gmail.com>
2026-07-16 11:41:09 -0300

    for loop

---
+++ Loops/for.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+#{0..10..2} -> {start..end..increment}
+for i in {0..10} # 0 1 2 3 4 5 6 7 8 9 10
+do
+    echo $i
+done
+
+for (( i=0; i<5; i++ ))
+do 
+    echo $i
+done
+
+#for VARIABLE in file1 file2 file3
+#do
+    #cmd1
+    #...
+    #cmdn
+#done
